c6fe443eb08b7e3150b56d72714def0c35ab80b2
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-01-27 00:12:25 -0500

    User service

--- src/server/api/routes.js
+++ src/server/api/routes.js
@@ -7,6 +7,10 @@ router.get('/test', (req, res) => {
     res.send('This is an test route');
 });
 
+// Import service-specific routes
+const userRoutes = require('./user.routes');
 
+// Use service-specific routes
+router.use('/user', userRoutes);
 
 module.exports = router;
